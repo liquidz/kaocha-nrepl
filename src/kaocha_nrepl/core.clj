50e5df15e7cc85fb7ce8a899e061e4ee6a056e56
liquidz <liquidz.uo@gmail.com>
2019-05-15 23:46:23 +0900

    Add progress reporter

--- src/kaocha_nrepl/core.clj
+++ src/kaocha_nrepl/core.clj
@@ -4,25 +4,39 @@
             [nrepl.misc :refer [response-for]]
             [nrepl.transport :as transport]))
 
+(def ^:private default-progress-format "Testing: %s")
+
 (defn- send! [m msg]
   (transport/send (:transport msg) (response-for msg m)))
 
-(defn- test-all-reply [msg]
-  (let [{:keys [config-file]} msg
-        config (cond-> {}
-                 config-file (assoc :config-file config-file))]
-    (-> (kaocha/run-all config)
-        (merge {:status :done})
-        (send! msg))))
+(defn- progress-reporter [msg test _test-plan]
+  (let [fmt (:progress-format msg default-progress-format)]
+    (send! {:out (format fmt (str (:kaocha.testable/id test)))} msg))
+  test)
+
+(defn- gen-config [msg]
+  (let [{:keys [config-file disable-progress-reporter]
+         :or {disable-progress-reporter false}} msg]
+    (cond-> {}
+      config-file
+      (assoc :config-file config-file)
+
+      (not disable-progress-reporter)
+      (assoc :kaocha.hooks/pre-test [(partial progress-reporter msg)]))))
 
 (defn- ensure-list [x]
   (cond-> x
     (not (sequential? x)) vector))
 
+(defn- test-all-reply [msg]
+  (-> (gen-config msg)
+      kaocha/run-all
+      (merge {:status :done})
+      (send! msg)))
+
 (defn- test-reply [msg]
-  (let [{:keys [config-file testable-ids]} msg
-        config (cond-> {}
-                 config-file (assoc :config-file config-file))
+  (let [{:keys [testable-ids]} msg
+        config (gen-config msg)
         run-args (some-> testable-ids ensure-list (concat [config]))]
     (if run-args
       (-> (apply kaocha/run run-args)
