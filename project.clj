b4fd0a533db69dea65af3c09eb8d5bd8f33df4bd
liquidz <liquidz.uo@gmail.com>
2019-05-27 17:01:01 +0900

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kaocha-nrepl "0.1.1-SNAPSHOT"
+(defproject kaocha-nrepl "0.1.1"
   :description "nREPL Middleware for kaocha"
   :url "https://github.com/liquidz/kaocha-nrepl"
   :license {:name "Eclipse Public License"
