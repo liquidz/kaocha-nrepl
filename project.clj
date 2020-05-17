5cb9cc9512e3d8cc719b4d8abffa0d71ac48f833
liquidz <liquidz.uo@gmail.com>
2020-05-18 06:05:56 +0900

    Bump kaocha

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[nrepl "0.7.0"]
-                 [lambdaisland/kaocha "1.0.629"
+                 [lambdaisland/kaocha "1.0.632"
                   :exclusions [org.clojure/clojure]]]
 
   :plugins [[lein-cloverage "1.1.2"]]
